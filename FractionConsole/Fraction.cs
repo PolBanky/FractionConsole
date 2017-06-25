4a011ed2a00c5816bbcf75b5e8306453a73ded48
PolBanky <pol.banky@gmail.com>
2017-06-26 01:39:39 +0300

    First commit

---
+++ FractionConsole/Fraction.cs
@@ -0,0 +1,91 @@
+﻿namespace FractionConsole
+{
+    class Fraction
+    {
+        int num, den;
+        
+        // КОНСТРУКТОР !!! 
+        public Fraction()
+        {   this.num = 1;
+            this.den = 1; }
+
+        // КОНСТРУКТОР !!! 
+        public Fraction(int num, int den)
+        {   this.num = num;
+            this.den = den;
+            Cancellation();
+        }
+        
+
+        // Свойства !!! 
+
+        public int Num
+        {get { return num; }}
+
+        public int Den
+        {get { return den; }}
+
+
+        // метод сокращения рациональной дроби
+        void Cancellation()
+        {
+            if (num != 0)
+            {
+                int m = den, n = num, ost = m % n;
+                // вычисление НОД(числителя, знаменателя)
+                // алгоритмом Евклида
+                while (ost != 0)
+                {
+                    m = n;
+                    n = ost;
+                    ost = m % n;
+                }
+                int nod = n;
+                if (nod != 1)
+                {
+                    num /= nod;
+                    den /= nod;
+                }
+            }
+        }           // void Cancellation()
+
+
+        // overload operator + 
+        public static Fraction operator +(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.den + b.num * a.den, a.den * b.den);
+        }   // Fraction operator + 
+
+        // overload operator - 
+        public static Fraction operator -(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.den - b.num * a.den, a.den * b.den);
+        }   // Fraction operator - 
+
+        // overload operator * 
+        public static Fraction operator *(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.num, a.den * b.den);
+        }   // Fraction operator * 
+
+        // overload operator / 
+        public static Fraction operator /(Fraction a, Fraction b)
+        {
+            return new Fraction(a.num * b.den, a.den * b.num);
+        }   // Fraction operator / 
+
+
+        // user-defined conversion from Fraction to double
+        public static implicit operator double(Fraction f)
+        {
+            return (double)((double)f.num / (double)f.den);
+        }   // conversion from Fraction to double 
+
+        // user-defined conversion from Fraction to double
+        public static implicit operator Fraction(int d)
+        {
+            return new Fraction( d, d);
+        }   // conversion from Fraction to double 
+
+    }       // class Fraction
+}           // namespace FractionConsole
---
+++ FractionConsole/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace FractionConsole
+{
+    class Test : ConLib.Lib
+    {
+        static void Main( )
+        {
+            int num, den;
+            char ch1 = ' ';
+
+            initialize( 120, 50);
+
+            Promt("Type the numerator of the first fraction: ", out num);            
+            Promt("Type the denominator of the first fraction: ", out den);
+            Fraction a = new Fraction(num,den);
+            s(" Fraction a = ", a.Num, "/", a.Den, " == ", (double)a);
+
+            do {
+                Promt("\nType the sign (+-*/) for arithmetic operation: ", out ch1);
+                s("Sign is: ", ch1);
+            } while ((ch1 != '+') & (ch1 != '-') & (ch1 != '*') & (ch1 != '/'));
+
+            Promt("\nType the numerator of the second fraction: ", out num);
+            Promt("Type the denominator of the second fraction: ", out den);
+            Fraction b = new Fraction(num, den);
+            s(" Fraction b = ", b.Num, "/", b.Den, " == ", (double)b);
+
+            Fraction d = 1;
+            Continue();
+
+            switch (ch1)
+            { 
+                case '+':
+                    d = a + b;
+                    s(" Fraction d = a + b  == ", d.Num, "/", d.Den, " == ", (double)d);
+                    break;
+                case '-':
+                    d = a - b;
+                    s(" Fraction d = a - b  == ", d.Num, "/", d.Den, " == ", (double)d);
+                    break;
+                case '*':
+                    d = a * b;
+                    s(" Fraction d = a * b  == ", d.Num, "/", d.Den, " == ", (double)d);
+                    break;
+                case '/':
+                    d = a / b;
+                    s(" Fraction d = a / b  == ", d.Num, "/", d.Den, " == ", (double)d);
+                    break;
+                default:
+                    s("Sign is false");
+                    break;
+            }   // switch (ch1) 
+                        
+            Exit();
+        }   // Main( )
+    }       // class Test 
+}           // namespace FractionConsole
